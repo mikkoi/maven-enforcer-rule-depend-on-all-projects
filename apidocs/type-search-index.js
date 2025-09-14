ac8e7bfa75e2f7691d930eaa129184f85de613ed
Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>
2025-09-14 21:14:16 +0200

    Site checkin for project Maven Enforcer Rule dependOnAllProjects

---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.mikkoi.maven_enforcer_plugin.rule","l":"DependOnAllProjects"}];updateSearchResults();
