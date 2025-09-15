7cc1ae04ce9905cfdb98b623ce975fcd067a0cdc
Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>
2025-09-15 19:00:13 +0200

    Add package-info.java files to both main and test

---
+++ src/main/java/com/github/mikkoi/maven_enforcer_plugin/rule/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Custom rule for the Maven Enforcer Plugin.
+ * dependOnAllProjects
+ */
+package com.github.mikkoi.maven_enforcer_plugin.rule;
---
+++ src/test/java/com/github/mikkoi/maven_enforcer_plugin/rule/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Custom rule for the Maven Enforcer Plugin.
+ * dependOnAllProjects
+ * Unit and integration tests.
+ */
+package com.github.mikkoi.maven_enforcer_plugin.rule;
